8cb08bab5b13cc5df8e3c8ed6a8db30f23000cab
jose <cov12003@byui.edu>
2016-06-03 13:21:23 -0700

    Add files via upload

---
+++ CIT230NavDoc.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>Start File</title>
+  <meta name="author" content="Jose Covarrubias">
+  <!-- TELLS PHONES NOT TO LIE ABOUT THEIR WIDTH -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+ </head>
+ <body>
+<header >
+  <h1>You Can I Can | covsite.com</h1>
+    <h2>Your Site Slogan (heading 2)</h2>
+</header>
+     
+     
+     
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">MS Tutorials</a>
+        	<ul>
+        		<li><a href="word.php">Word</a></li>
+        		<li><a href="excel.php">Excel</a></li>
+        		<li><a href="powerPoint.php">PowerPoint</a></li>
+        	</ul>
+        
+        </li>
+        <li><a href="#">Auto</a>
+        	<ul>
+        		<li><a href="">Repairs</a></li>
+        		<li><a href="">Maintenance</a></li>
+        	</ul>
+        </li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact Us</a></li>
+
+    </ul>
+</nav>
+     
+     
+     
+   <main>
+     <h1>Home</h1>
+      
+     <p>The Page title should MATCH the menu item! So if the menu item says "About Us" then the page title should say "About Us" This follows two design principles called "Wayfinding" and "Consistency".</p>
+      
+   </main>
+
+     
+     
+   <footer>
+   <script src="/javascript/analytics.js"></script>
+       <p>&copy;2015 &bull; Paul Cheney &bull; <a href="plan.php">Site Plan Link</a> &bull; <a href="presentation.php">Teaching Presentation</a></p>
+   
+   </footer>
+
+ </body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+My site Home page.
+-->
+<html  lang="en-us">
+   <head>
+      <meta charset="UTF-8">
+      <title>Home | covsite.com</title>
+      <meta name="autor" content="Jose Covarrubias">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+  <body id="home">
+    <div>
+        <header role="banner">
+            <div>
+                <h1>YoucanIcan.covsite.com</h1>  
+            </div>
+       </header>
+       <nav role="navigation">
+           <div>
+               
+           </div>
+       </nav>
+       <main role="main">
+           <div>
+               <h1>Home</h1>
+               <p>This website will provide you with video tutorials that
+                  can make your life easier</p>
+           </div>
+       </main>
+       <aside role="complementary">
+           <div>
+                 
+           </div>
+       </aside>
+       <footer role="contentinfo">
+           <div>
+              <ul>
+                 <li>Site Plan</li>
+                 <li>Teaching Presentation</li>
+              </ul>   
+           </div>
+       </footer>
+    </div>
+  </body>
+</html>
