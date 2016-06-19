1a98cb4687499ec6c2efc389d5c41a025688b68b
josecovarrubias <josecovarrubias@joses-imac.domain>
2016-06-19 13:08:48 -0700

    styling CSS

--- modules/header.php
+++ modules/header.php
@@ -1,9 +1,9 @@
 
 
 
-<a href="/index.php"><img src="/siteImages/YoucanLogo.png" alt="Logo Image" title="Click on the Logo to go to home page."></a>
+<a id="logoPic" href="/index.php"><img src="/siteImages/YoucanLogo.png" alt="Logo Image" title="Click on the Logo to go to home page."></a>
 <link rel="stylesheet" href="/css/youCanTemplateFont.css">
-<h2>The tutorials that will save you money<br>
+<h2 id="headerH">The tutorials that will save you money
     and teach you one or two things along the way.
 </h2>
 <!--<p><b>^ This is the header area. ^</b></p>
