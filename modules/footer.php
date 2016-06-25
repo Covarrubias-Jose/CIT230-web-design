6e52c4442dadd8c9b592d5e2e2bc3bf8340a353f
josecovarrubias <josecovarrubias@joses-imac.domain>
2016-06-24 19:53:06 -0700

    updates june 24

--- modules/footer.php
+++ modules/footer.php
@@ -2,14 +2,14 @@
 <hr>-->
 <link rel="stylesheet" href="/css/youCanTemplateFont.css">
     <!--<p><b>This is my footer area.</b></p>-->
-<ul id="footerNav">  
+<ul class="footerNav">  
     <li><a href="/plandemo/plan.html">Site Plan</a></li>
     <li><a href="teachingpresentation.php">Teaching Presentation</a></li>
     <li><a href="/aboutus/aboutUs.php">About Us</a></li>
     <li><a href="/contactus/contact.php">Contact Us</a>
         <ul>
-            <li><a href="requesttutorial.php">Request a tutorial.</a></li>
-            <li><a href="submitvideo.php">Submit a video.</a></li>
+            <li><a href="requesttutorial.php">Request a tutorial</a></li>
+            <li><a href="submitvideo.php">Submit a video</a></li>
         </ul>
     </li>
 </ul>
--- modules/navigation.php
+++ modules/navigation.php
@@ -1,7 +1,7 @@
 
 <!--<p><b>This area is for the navigation</b></p>-->
 <nav>
-    <ul class="primary-nav">
+<ul class="primary-nav">
     <li><a href="/index.php">Main Page</a></li>
     <li><a href="/rotator/imageRotator.php">Image Carousel</a></li>
     <li><a>Microsoft Office Tutorials</a>
@@ -14,29 +14,18 @@
     <li><a>Home</a>
       <ul>
          <li><a>Ideas</a>
-             <ul>
-                 <li><a>For the outside.</a></li>
-             </ul>
-         </li>
-         <li><a>Maintenance</a>
-            <ul>
-                 <li><a href="/home/homemaintenance/acManitenance.php">AC Maintenance</a></li>
-            </ul>
+         <li><a>For the outside</a></li>
+         <li><a>Maintenance</a></li>
+         <li><a href="/home/homemaintenance/acManitenance.php">AC Maintenance</a></li>
          </li>
       </ul>
     </li>
     <li><a>Auto</a>
        <ul>
-          <li><a>Repairs</a>
-              <ul>
-                  <li><a>Tail lights</a></li>
-              </ul>
-          </li>
-          <li><a>Maintenance</a>
-              <ul>
-                  <li><a href="/auto/maintenance/cabinFilter.php">Cabin air filter</a></li>
-              </ul>
-          </li>
+          <li><a>Repairs</a></li>
+          <li><a>Tail lights</a></li>
+          <li><a>Maintenance</a></li>
+          <li><a href="/auto/maintenance/cabinFilter.php">Cabin air filter</a></li>
        </ul>
     </li>
     <li><a>Other Tutorials</a>
