779cd77db202892bd07682db3d092a28631619b6
josecovarrubias <josecovarrubias@joses-imac.domain>
2016-07-02 23:26:21 -0700

    site updates

--- modules/header.php
+++ modules/header.php
@@ -1,7 +1,7 @@
 
 
     <!--picture for the site Logo.-->
-<a id="logoPic" href="/index.php"><img src="/siteImages/greyLogo.png" width="280" 
+    <a id="logoPic" href="/index.php"><img src="/siteImages/YoucanLogo.png" width="280" 
    height="230" alt="Logo Image" title="Click on the Logo to go to home page."></a>
     
     <!--Link to the CSS file.-->
