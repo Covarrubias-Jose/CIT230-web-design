1d6c2bf5e9d68cf61a7ceb2ae5fbf860873f9b54
josecovarrubias <josecovarrubias@joses-imac.domain>
2016-07-08 01:03:18 -0700

    css

--- modules/footer.php
+++ modules/footer.php
@@ -9,10 +9,6 @@
             <li><a href="teachingpresentation.php">Teaching Presentation</a></li>
             <li><a href="/aboutus/aboutUs.php">About Us</a></li>
             <li><a href="/contactus/contact.php">Contact Us</a>
-                <ul>
-                    <li><a href="requesttutorial.php">Request a tutorial</a></li>
-                    <li><a href="submitvideo.php">Submit a video</a></li>
-                </ul>
             </li>
         </ul>
         <br>
@@ -21,3 +17,8 @@
             <p>&copy;2016 covsite.com</p>
         </div>
 </div>
+
+<!--                <ul>
+                    <li><a href="requesttutorial.php">Request a tutorial</a></li>
+                    <li><a href="submitvideo.php">Submit a video</a></li>
+                </ul>-->
