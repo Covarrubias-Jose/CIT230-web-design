35fa32a8f0d2f8c3ccb7139f57256c0adc3a9844
josecovarrubias <josecovarrubias@joses-imac.domain>
2016-07-18 12:58:08 -0700

    salmon

--- salmon/modules/navigation.php
+++ salmon/modules/navigation.php
@@ -3,8 +3,8 @@
 <h2>Navigation area</h2>
 <ul>
     <li><a href="">Home</a></li>
-    <li><a href="#">page</a></li>
-    <li><a href="#">page</a></li>
+    <li><a href="#">Adventures</a></li>
+    <li><a href="#">Guides</a></li>
     <li><a href="#">page</a></li>
     <li><a href="#">page</a></li>
 </ul>
